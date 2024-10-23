4578f58dc5d9f895aeb93be66b3e1298bc2d82ee
Carlos Capote Pérez-Andreu <carlos.capote@hawara.es>
2024-10-23 21:45:25 +0100

    download the robots.txt file and store it locally

---
+++ app/Jobs/Boc/DownloadRobots.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Jobs\Boc;
+
+use App\Http\BocUrl;
+use App\Jobs\DownloadPage;
+use App\Models\Page;
+use Illuminate\Support\Facades\Storage;
+
+class DownloadRobots extends DownloadPage
+{
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        parent::__construct(
+            url: BocUrl::Robots->value,
+            name: BocUrl::Robots->name,
+            root: BocUrl::Root->value
+        );
+    }
+
+    /**
+     * Extract the links of this page.
+     */
+    protected function extractLinks(int $pageId): void
+    {
+        $page = Page::findOrFail($pageId);
+        $content = mb_convert_encoding($page->content, 'UTF-8', 'UTF-8');
+
+        Storage::disk('local')
+            ->put('robots.txt', $content);
+    }
+}
