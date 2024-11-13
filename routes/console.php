ef751b8937d27613700a5f7937a3a6fb5d7e7422
Carlos Capote Pérez-Andreu <carlos.capote@hawara.es>
2024-11-13 19:49:51 +0000

    run composer update

--- routes/console.php
+++ routes/console.php
@@ -3,10 +3,12 @@
 use App\Jobs;
 use Illuminate\Support\Facades\Schedule;
 
-//Schedule::job(new Jobs\Boc\DownloadRobots)->dailyAt('00:00');
-//Schedule::job(new Jobs\Boc\DownloadArchive)->dailyAt('00:05');
-//Schedule::job(new Jobs\Boc\FollowLinksFoundInArchive)->everyMinute();
-//Schedule::job(new Jobs\Boc\FollowLinksFoundInYearIndex)->everyMinute();
-Schedule::job(new Jobs\Boc\FollowLinksFoundInBulletinIndex)->everyMinute();
+Schedule::job(new Jobs\Boc\DownloadRobots)->dailyAt('00:00');
+Schedule::job(new Jobs\Boc\DownloadArchive)->dailyAt('00:05');
+Schedule::job(new Jobs\Boc\DownloadYearIndex(App\Models\Link::find(45)))->dailyAt('00:10');
+Schedule::job(new Jobs\Boc\ExtractLinksFromYearIndex(App\Models\Page::find(176348), recreate: true))->everyMinute();
+Schedule::job(new Jobs\Boc\FollowLinksFoundInArchive)->hourly();
+Schedule::job(new Jobs\Boc\FollowLinksFoundInYearIndex)->hourly();
+Schedule::job(new Jobs\Boc\FollowLinksFoundInBulletinIndex)->hourly();
 Schedule::job(new Jobs\TakeSnapshot)->everyFiveMinutes();
 Schedule::command('horizon:snapshot')->everyFiveMinutes();
